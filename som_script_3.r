0a4f9612e7bb4143ae1b1d121dfc18a1aa1470fb
Aeesha <dolapo.toye@yahoo.com>
2020-11-18 22:56:00 +0200

    Update to the algorithms

--- som_script_3.r
+++ som_script_3.r
@@ -1,4 +1,4 @@
-#one holdout method 
+#one holdout method
 library(kohonen)
 library(gridExtra)
 library(grid)
@@ -6,89 +6,63 @@ library(grid)
 #read the data
 data <- read.csv("readtext.csv")
 
-#drop the first(the ID) and last(the label) column
-data_train <- data[, -c(1, 13)]
-labels <- data[, c(13)]
-data_train_matrix <- as.matrix(scale(data_train))
-
-#SOM
-som_grid <- somgrid(xdim = 3, ydim = 3, topo = "hexagonal")
-som_model <- som(data_train_matrix,
-                  grid = som_grid, rlen = 800,
-                  alpha = c(0.05, 0.01), keep.data = TRUE)
-
-print(som_model)
-
 ##### Measures ########
-iterations <- 10
+iterations <- 5
+#drop the first column because it is the voice ID
 data_total <- data[, -c(1)]
 accuracy_table <- NULL
 #Loop through iterations
 for (iteration in 1:iterations) {
-    print("Iteration")
-    print(iteration)
-accuracy <- 0
-prediction_table <- NULL
-for (i in 1:37) {
+    print(paste("Iteration", iteration, sep = ""))
+    accuracy <- 0
+    prediction_table <- NULL
+    for (i in 1:37) {
+    # Select sample no i as test, and all others as train.
     train <- data_total [-c(i), ]
     test <- data_total [i, ]
-
-    # Normalization
+    #Extract the train data by excluding the 12 column which is label
     trainX <- scale(train[, -12])
     testX <- scale(test[, -12],
                 center = attr(trainX, "scaled:center"),
                 scale = attr(trainX, "scaled:scale"))
+    #Extract the label (the 12th column) for both the train and test
     trainY <- factor(train[, 12])
     Y <- factor(test[, 12])
     train_Y_matrix <- classvec2classmat(trainY)
-    #print(classmat2classvec(xx))
+    #model
     map1 <- xyf(trainX,
                 train_Y_matrix,
                 grid = somgrid(3, 3, "hexagonal"),
                 rlen = 100)
 
+    #make predictions
     pred <- predict(map1, newdata = testX, whatmap = 1)
-    print(pred$predictions)
+    #print(pred$predictions)
     predicted_y <- classmat2classvec(pred$predictions[[2]])
     #print(predicted_y)
     #print(data.frame(i, Y, predicted_y))
     if (Y == predicted_y) {
         accuracy <- accuracy + 1
     }
-   prediction_table <- rbind(prediction_table, data.frame(i, Y, predicted_y))
+    #populate the prediction table
+    prediction_table <- rbind(prediction_table, data.frame(i, Y, predicted_y))
+    }
 
-    #confusion_matrix <- table(Predicted = pred$predictions[[2]], Actual = Y)
-    # som_grid <- somgrid(3,3, "hexagonal")
-    # som_model <- som(trainX, som_grid)
-    # som.prediction <- predict(som_model, newdata = testX)
-    # error.df <- data.frame(Y,
-    #                    predicted = som.prediction$unit.classif)
-    #                    print(error.df)
-}
-print(prediction_table)
-accuracy_value <- accuracy / 37
-print(accuracy_value)
-#populate accuracy table
-accuracy_table <- rbind(accuracy_table, data.frame(iteration, accuracy_value))
+    print(prediction_table)
+    #calculate the accuracy value
+    accuracy_value <- accuracy / 37
+    print(accuracy_value)
+    #populate accuracy table
+    accuracy_table <- rbind(accuracy_table, data.frame(iteration, accuracy_value))
 }
 print(accuracy_table)
 
 
 
-#plot the graph
+#save to a file
 pdf("C:/Users/Aeesha/Documents/CMU/Research/Code/Research_Project/OneHoldOutEvaluation.pdf", height = 15, width = 10)
 title <- paste("Accuracy Table")
 grid::grid.text(title, x = (0.5), y = (0.99))
 grid.table(accuracy_table)
 plot(accuracy_table)
-# grid::grid.newpage()
-# title <- paste("Confusion Matrix Accuracy")
-# grid::grid.text(title,x = (0.5), y = (0.99))
-# grid.table(confusion_table)
-# plot(confusion_table)
-
 dev.off()
-
-
-
-
--- som_script_4.r
+++ som_script_4.r
@@ -9,20 +9,19 @@ data <- read.csv("readtext.csv")
 
 #drop the first(the ID) and last(the label) column
 data_train <- data[, -c(1, 13)]
-labels <- data[, c(13)]
 data_train_matrix <- as.matrix(scale(data_train))
 
-#SOM
+#Specify the grid to use in the SOM model
 som_grid <- somgrid(xdim = 3, ydim = 3, topo = "hexagonal")
 
 #print(som_model$grid$pts[1, ]) #prints row 1 and col 2 in the pts table
 
-##### Measures ########
-iterations <- 3 #10
-total_clusters <- 9 #9
-data_total <- data[, -c(1)]
-accuracy_table <- NULL
+#declare the number of iterations
+iterations <- 5
+#declare the total number of clusters whihc is xdim*ydim
+total_clusters <- 9
 
+#create the clusterDetails class with samples_in_cluster and cluster_centroid as properties
 setClass("clusterDetails", slots = list(samples_in_cluster = "integer",
                                         cluster_centroid = "numeric"))
 clusters_list <- vector("list", iterations)
@@ -30,88 +29,91 @@ SSE <- vector("list", total_clusters)
 diff <- vector("list", total_clusters)
 #Loop through iterations
 for (iteration in 1:iterations) {
-    print("Iteration")
-    print(iteration)
-
     #som training
     som_model <- som(data_train_matrix,
                   grid = som_grid, rlen = 800,
                   alpha = c(0.05, 0.01), keep.data = TRUE)
 
-    for (j in 1:total_clusters) { #9 is the number of clusters (3*3)
-        samples_in_cluster_i <- which(som_model$unit.classif == j) #store the samples in this cluster
-        centroid_cluster_i <- som_model$grid$pts[j, ]
-        #store samples in cluster 1
-        #data <- list(samples = samples_in_cluster_i,
-        #centroid = som_model$grid$pts[j, ])
+    for (j in 1:total_clusters) {
+        #store the samples in this cluster
+        samples_in_cluster_i <- which(som_model$unit.classif == j)
+        #extract the centroid for this cluster and store it
+        centroid_cluster_i <- som_model$codes[[1]][j, ]
+        #create an object of the clusterDetails and pass in these values
         obj <- new("clusterDetails", samples_in_cluster = samples_in_cluster_i,
                     cluster_centroid = centroid_cluster_i)
+        #save this object in the cluster list
         clusters_list[[iteration]][[j]] <- obj
         #print(obj)
         #print(clusters_list[[1]][[1]]@cluster_centroid)
     }
 }
- #print(clusters_list[[1]][[1]]@cluster_centroid)
 print("Done1")
 
 result_table <- NULL
 for (clust in 1:total_clusters) {
-    print(paste("clust no ", clust, sep = ""))
+    print(paste("cluster no:", clust, sep = ""))
     SSE[[clust]] <- 0
     diff[[clust]] <- 0
+    #retrieve the centroid for this cluster in iteration 1
     center <- clusters_list[[1]][[clust]]@cluster_centroid
+    #retrieve the samples in this cluster in iteration 1
     samples_in_cluster_iter1 <- clusters_list[[1]][[clust]]@samples_in_cluster
     for (j in 2:iterations) {
         min_dist <- 10000
+        print(paste("Iteration: ", j, sep = ""))
         for (k in 1:total_clusters) {
-            #find the dist btw cluster 1 in iteration 1 and
-            #clusters in iteration 2
+            #find the dist between this cluster in iteration 1 and all clusters in iteration 2
             center2 <- clusters_list[[j]][[k]]@cluster_centroid
             d <- dist(rbind(center, center2))
-            dist <- dist_get(d, "center2", "center")
-            print("cluster")
-            print(k)
-            print("iteration")
-            print(j)
-            #print(center)
-            #print(clusters_list[[j]][[k]]@cluster_centroid)
-            print(paste("d dist is: ", dist, sep = ""))
-            print(paste("d min dist is: ", min_dist, sep = ""))
+            dist <- d[1]
+            print(paste("We are checking iteration 1 and cluster ",
+            clust, " with iteration ", j, " and cluster ", k,
+             ". The dist is: ", dist, sep = ""))
             if (dist < min_dist) {
                 min_dist <- dist
                 min_clust_index <- k
             }
+            print(paste("d min dist at this point is: ", min_dist, sep = ""))
         }
-        print(paste("The final min dist is ", min_dist, " and the index is ", min_clust_index, sep=""))
+        print(paste("The final min dist is ", min_dist,
+        " and the index is ", min_clust_index, sep = ""))
+
         SSE[[clust]] <- SSE[[clust]] + min_dist
-        print(paste("SSE for cluster ", clust, " at iteration ", j, " is ", SSE[[clust]], sep=""))
-        #get the cluster details of the closest cluster
+        print(paste("SSE for cluster ", clust,
+         " in iteration 1 with the closest cluster at iteration ",
+         j, " is ", SSE[[clust]], sep = ""))
+        #get the cluster details of the closest cluster using the index saved above
         samples_in_closest_cluster <-
         clusters_list[[j]][[min_clust_index]]@samples_in_cluster
         #get the no of samples that are different between the cluster data
+        #samples in first clust but not in second
         a <- length(setdiff(samples_in_cluster_iter1,
-                            samples_in_closest_cluster)) #samples in first clust
+                            samples_in_closest_cluster))
+        #samples in second clust but not in first
         b <- length(setdiff(samples_in_closest_cluster,
-                            samples_in_cluster_iter1)) #samples in first clust
+                            samples_in_cluster_iter1))
         diff[[clust]] <-  diff[[clust]] + a + b
-        print(paste("Diff for cluster ", clust, " at iteration ", j, " is ", diff[[clust]], sep=""))
+        print(paste("Diff for cluster ", clust,
+        " in iteration 1 with the closest cluster at iteration ",
+         j, " is ", a + b, sep = ""))
         print("SAMPLES IN THE CLUSTER ")
         print(samples_in_cluster_iter1)
         print(samples_in_closest_cluster)
     }
-    SS_Error <- SSE[[clust]]
+    ss_error <- SSE[[clust]]
     diff_in_samples <- diff[[clust]]
-    print(SSE[[clust]])
+    #populate the result_table
     result_table <- rbind(result_table,
-                            data.frame(clust, SS_Error, diff_in_samples))       
+                            data.frame(clust, ss_error, diff_in_samples))
 }
 
-#calculate average 
-SSE_avg <- mean(result_table[["SS_Error"]])
+#calculate average
+sse_avg <- mean(result_table[["ss_error"]])
 diff_avg <- mean(result_table[["diff_in_samples"]])
 
 print(result_table)
 #print
 print(paste("Number of Iterations: ", iterations, sep = ""))
-print(paste("The SSE average is ", SSE_avg, sep = ""))
+print(paste("The SSE average is ", sse_avg, sep = ""))
 print(paste("The Difference in samples average is ", diff_avg, sep = ""))
